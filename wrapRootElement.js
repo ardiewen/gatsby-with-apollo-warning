22c179e67a807d02dc2714ba95a714e7c3291798
Ardie Wen <ardie.wen@hunterlily.co>
2021-06-03 23:41:26 +0000

    feat: implement apollo client and provider in ssr

---
+++ wrapRootElement.js
@@ -0,0 +1,22 @@
+import React from "react"
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client"
+import fetch from "cross-fetch"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://countries.trevorblades.com",
+    fetch,
+  }),
+})
+
+const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+)
+
+export default wrapRootElement
